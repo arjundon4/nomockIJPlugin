2977a71c54802f9e00366234197fe9cd86edd277
Arjun Vinod <arjunvinod3@gmail.com>
2020-10-27 15:42:11 +0530

    Initial commit

---
+++ src/main/java/com/sap/sf/nomockfw/MainAction.java
@@ -0,0 +1,23 @@
+package com.sap.sf.nomockfw;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class MainAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        String command = "./gradlew.bat clean";
+        try
+        {
+            Runtime.getRuntime().exec(command);
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
